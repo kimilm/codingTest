e1c78cc1546f4625618b8afef1561e27c0fe904a
Kimilm <gnlfla7416@gmail.com>
2021-05-06 12:50:54 +0900

    Update Main.java

--- src/codingTest/Main.java
+++ src/codingTest/Main.java
@@ -14,8 +14,9 @@ public class Main {
 		// System.out.println(i + ": " + solution.solution020902(i));
 
 //		System.out.println(solution.solution021501(new int[] { 1, 2, 3, 9, 10, 12 }, 7));
-		System.out.println(solution.solution040601(new int[][] {{1,2,3,5},{5,6,7,8},{4,3,2,1}}));     
-		
+//		System.out.println(solution.solution040601(new int[][] {{1,2,3,5},{5,6,7,8},{4,3,2,1}}));
+//		System.out.println(solution.solution050401(new String[] { "1195524421", "97674223", "119" }));
+		System.out.println(solution.solution050402(new String[] { "11", "12", "119" }));
 
 //		System.out.println(solution.solution020801(new int [] {1, 1, 9, 1, 1, 1}, 1));
 //		System.out.println(solution.solution020801(new int [] {1, 1, 9, 1, 1, 1}, 2));
@@ -89,120 +90,126 @@ class Solution {
 		return answer;
 	}
 
-	int solution040501(String numbers) {		// 소수 찾기
+	int solution040501(String numbers) { // 소수 찾기
 		Set<Integer> numberSet = new HashSet<>();
-		String [] tokens = numbers.split("");
+		String[] tokens = numbers.split("");
 		int len = tokens.length + 1;
-		
+
 		Arrays.sort(tokens, Collections.reverseOrder());
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < tokens.length; ++i) {
+		for (int i = 0; i < tokens.length; ++i) {
 			sb.append(tokens[i]);
 		}
 		int max = Integer.parseInt(sb.toString());
 
-		boolean [] prime = new boolean[max + 1];
+		boolean[] prime = new boolean[max + 1];
 		prime[0] = true;
 		prime[1] = true;
-		
-		for(int i = 2; i < prime.length; ++i) {
+
+		for (int i = 2; i < prime.length; ++i) {
 			if (!prime[i])
 				for (int j = i * 2; j < prime.length; j += i)
 					prime[j] = true;
 		}
-		
-		for(int i = 1; i < len; ++i) {
+
+		for (int i = 1; i < len; ++i) {
 			Permutation main = new Permutation(tokens, i);
 			numberSet.addAll(main.perm(0));
 		}
-		
+
 		int answer = 0;
-		
+
 		for (Integer number : numberSet) {
 			if (!prime[number])
 				++answer;
 		}
-		
-	    return answer;
+
+		return answer;
 	}
-	
+
 	// for solution040501 소수 찾기
 	// https://redscreen.tistory.com/168
 	class Permutation {
-		 
-	    private int n; // nPr의 n
-	    private int r; // nPr의 r
-	    private ArrayList<String> itemList;
-	    private String[] res;
-	    private List<Integer> result;
-	    
-	    // 초기화
-	    public Permutation(String [] intArr, int r){
-	        this.r = r;             // nPr의 r
-	        this.n = intArr.length; // nPr의 n
-	        this.res = new String[r];  // 결과값 배열
-	        this.result = new ArrayList<>();
-	        
-	        // 아이템이 들어갈 리스트
-	        itemList = new ArrayList<String>();
-	        for(String item : intArr)
-	            itemList.add(item);
-	    }
-	    
-	    public List<Integer> perm(int depth){
-	        perm(itemList, 0);
-	        return result;
-	    }
-	 
-	    public void perm(ArrayList<String> itemList, int depth) {
-	        
-	    	// depth가 0부터 시작했을 경우 depth == r에서 리턴
-	        if (depth == r) {
-	        	StringBuilder sb = new StringBuilder();
-	        	for(String token : res)
-	        		sb.append(token);
-	            result.add(Integer.parseInt(sb.toString())); 
-	            //System.out.println(Arrays.toString(res));
-	            return;
-	        }
-	        
-	        for (int i = 0; i < n-depth; i++){
-	            res[depth] = itemList.remove(i); // 아이템 선택 + 리스트에서 제거
-	            perm(itemList, depth+1);         // 재귀호출
-	            itemList.add(i, res[depth]);     // 제거된 아이템 복원
-	        }
-	    }
+
+		private int n; // nPr의 n
+		private int r; // nPr의 r
+		private ArrayList<String> itemList;
+		private String[] res;
+		private List<Integer> result;
+
+		// 초기화
+		public Permutation(String[] intArr, int r) {
+			this.r = r; // nPr의 r
+			this.n = intArr.length; // nPr의 n
+			this.res = new String[r]; // 결과값 배열
+			this.result = new ArrayList<>();
+
+			// 아이템이 들어갈 리스트
+			itemList = new ArrayList<String>();
+			for (String item : intArr)
+				itemList.add(item);
+		}
+
+		public List<Integer> perm(int depth) {
+			perm(itemList, 0);
+			return result;
+		}
+
+		public void perm(ArrayList<String> itemList, int depth) {
+
+			// depth가 0부터 시작했을 경우 depth == r에서 리턴
+			if (depth == r) {
+				StringBuilder sb = new StringBuilder();
+				for (String token : res)
+					sb.append(token);
+				result.add(Integer.parseInt(sb.toString()));
+				// System.out.println(Arrays.toString(res));
+				return;
+			}
+
+			for (int i = 0; i < n - depth; i++) {
+				res[depth] = itemList.remove(i); // 아이템 선택 + 리스트에서 제거
+				perm(itemList, depth + 1); // 재귀호출
+				itemList.add(i, res[depth]); // 제거된 아이템 복원
+			}
+		}
 	}
-	
+
 	class Land implements Comparable<Land> {
 		private int col;
 		private int val;
-		
-		public Land (int col, int val) {
+
+		public Land(int col, int val) {
 			this.col = col;
 			this.val = val;
 		}
+
 		public int getCol() {
 			return col;
 		}
+
 		public void setCol(int col) {
 			this.col = col;
 		}
+
 		public int getVal() {
 			return val;
 		}
+
 		public void setVal(int val) {
 			this.val = val;
 		}
+
 		@Override
 		public int compareTo(Land o) {
-			return  o.getVal() - this.val;
+			return o.getVal() - this.val;
 		}
 	}
-	
+
 	int solution040601(int[][] land) {	// 땅따먹기
 		Land [] sorted = new Land[4];
 		int [] sum = new int [4];
+		sum = land[0];
 		
 		for (int i = 0; i < 4; ++i) {
 			sorted[i] = new Land(i, sum[i]);
@@ -230,5 +237,24 @@ class Solution {
 		return sum[3];
 	}
 
+	boolean solution050401(String[] phone_book) {	// 전화번호 목록
+		boolean answer = true;
+        String prefix;
+        
+        Arrays.sort(phone_book);
+        
+        for(int i = 1; i < phone_book.length && answer; ++i) {
+        	prefix = phone_book[i - 1];
+        	
+        	if (prefix.length() < phone_book[i].length()) {
+        		if (phone_book[i].substring(0, prefix.length()).equals(prefix)) {	// String.startWith()
+        			answer = false;
+        		} else {
+        			++i;
+        		}
+        	}
+        }
+        
+        return answer;
+    }
 }
-
