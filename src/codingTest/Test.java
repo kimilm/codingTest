d02d9aad8307118a0a22e07b3512166ce4c2fed1
Kimilm <gnlfla7416@gmail.com>
2021-05-10 18:30:59 +0900

    Update Main

--- src/codingTest/Test.java
+++ src/codingTest/Test.java
@@ -2,286 +2,9 @@ package codingTest;
 
 import java.util.*;
 
-import codingTest.Solution.Land;
+import codingTest.Solution.Point;
 
-public class Main {
-	public static void main(String[] args) {
-		Solution solution = new Solution();
-
-		int[] arr = { 1, 2, 3, 4, 5 };
-
-		// for(int i = 0; i < 20; ++i)
-		// System.out.println(i + ": " + solution.solution020902(i));
-
-//		System.out.println(solution.solution021501(new int[] { 1, 2, 3, 9, 10, 12 }, 7));
-//		System.out.println(solution.solution040601(new int[][] {{1,2,3,5},{5,6,7,8},{4,3,2,1}}));
-//		System.out.println(solution.solution050401(new String[] { "1195524421", "97674223", "119" }));
-//		System.out.println(solution.solution050801("one4seveneight"));
-//		System.out.println(Arrays.toString(solution.solution050802(new String[][] {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"}, {"PXOPX", "OXOXP", "OXPXX", "OXXXP", "POOXX"},{"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},{"PXPX0P", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}})));
-//		System.out.println(solution.solution0508032(8, 2, new String [] {"D 2","C","U 3","C","D 4","C","U 2"}));
-//		System.out.println(solution.solution0508032(8, 0, new String [] {"C", "C", "D 1", "C", "D 1", "C", "C"}));
-		System.out.println(solution.solution0508032(8, 0, new String [] {"C", "C", "D 1", "C", "U 1", "C", "C"}));
-
-//		System.out.println(solution.solution020801(new int [] {1, 1, 9, 1, 1, 1}, 1));
-//		System.out.println(solution.solution020801(new int [] {1, 1, 9, 1, 1, 1}, 2));
-//		System.out.println(solution.solution020801(new int [] {1, 1, 9, 1, 1, 1}, 3));
-//		System.out.println(solution.solution020801(new int [] {1, 1, 9, 1, 1, 1}, 4));
-//		System.out.println(solution.solution020801(new int [] {1, 1, 9, 1, 1, 1}, 5));
-
-//		System.out.println(Arrays.toString());
-	}
-}
-
-class Solution {
-
-	// 02 15
-	public int solution021501(int[] scoville, int K) { // 더 맵게
-		PriorityQueue<Integer> heap = new PriorityQueue<>();
-
-		for (int i = 0; i < scoville.length; ++i) {
-			heap.add(scoville[i]);
-		}
-
-		int answer = 0;
-
-		int littleSpicy;
-		int moreSpicy;
-
-		while (heap.peek() < K) {
-			if (heap.size() < 2)
-				return -1;
-			littleSpicy = heap.poll();
-			moreSpicy = heap.poll();
-
-			heap.add(littleSpicy + moreSpicy * 2);
-
-			++answer;
-		}
-
-		return answer;
-	}
-
-	boolean solution021502(String s) { // 올바른 괄호
-		Stack<Boolean> stack = new Stack<>();
-
-		if (s.charAt(0) == ')')
-			return false;
-
-		for (int i = 0; i < s.length(); ++i) {
-			switch (s.charAt(i)) {
-			case '(':
-				stack.push(false);
-				break;
-			case ')':
-				if (stack.isEmpty()) {
-					return false;
-				}
-				stack.pop();
-				break;
-			}
-		}
-
-		return stack.isEmpty();
-	}
-
-	int solution021503(int n) { // 다음 큰 숫자
-		int bits = Integer.bitCount(n);
-		int answer = n + 1;
-
-		while (Integer.bitCount(answer) != bits)
-			++answer;
-
-		return answer;
-	}
-
-	int solution040501(String numbers) { // 소수 찾기
-		Set<Integer> numberSet = new HashSet<>();
-		String[] tokens = numbers.split("");
-		int len = tokens.length + 1;
-
-		Arrays.sort(tokens, Collections.reverseOrder());
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < tokens.length; ++i) {
-			sb.append(tokens[i]);
-		}
-		int max = Integer.parseInt(sb.toString());
-
-		boolean[] prime = new boolean[max + 1];
-		prime[0] = true;
-		prime[1] = true;
-
-		for (int i = 2; i < prime.length; ++i) {
-			if (!prime[i])
-				for (int j = i * 2; j < prime.length; j += i)
-					prime[j] = true;
-		}
-
-		for (int i = 1; i < len; ++i) {
-			Permutation main = new Permutation(tokens, i);
-			numberSet.addAll(main.perm(0));
-		}
-
-		int answer = 0;
-
-		for (Integer number : numberSet) {
-			if (!prime[number])
-				++answer;
-		}
-
-		return answer;
-	}
-
-	// for solution040501 소수 찾기
-	// https://redscreen.tistory.com/168
-	class Permutation {
-
-		private int n; // nPr의 n
-		private int r; // nPr의 r
-		private ArrayList<String> itemList;
-		private String[] res;
-		private List<Integer> result;
-
-		// 초기화
-		public Permutation(String[] intArr, int r) {
-			this.r = r; // nPr의 r
-			this.n = intArr.length; // nPr의 n
-			this.res = new String[r]; // 결과값 배열
-			this.result = new ArrayList<>();
-
-			// 아이템이 들어갈 리스트
-			itemList = new ArrayList<String>();
-			for (String item : intArr)
-				itemList.add(item);
-		}
-
-		public List<Integer> perm(int depth) {
-			perm(itemList, 0);
-			return result;
-		}
-
-		public void perm(ArrayList<String> itemList, int depth) {
-
-			// depth가 0부터 시작했을 경우 depth == r에서 리턴
-			if (depth == r) {
-				StringBuilder sb = new StringBuilder();
-				for (String token : res)
-					sb.append(token);
-				result.add(Integer.parseInt(sb.toString()));
-				// System.out.println(Arrays.toString(res));
-				return;
-			}
-
-			for (int i = 0; i < n - depth; i++) {
-				res[depth] = itemList.remove(i); // 아이템 선택 + 리스트에서 제거
-				perm(itemList, depth + 1); // 재귀호출
-				itemList.add(i, res[depth]); // 제거된 아이템 복원
-			}
-		}
-	}
-
-	class Land implements Comparable<Land> {
-		private int col;
-		private int val;
-
-		public Land(int col, int val) {
-			this.col = col;
-			this.val = val;
-		}
-
-		public int getCol() {
-			return col;
-		}
-
-		public void setCol(int col) {
-			this.col = col;
-		}
-
-		public int getVal() {
-			return val;
-		}
-
-		public void setVal(int val) {
-			this.val = val;
-		}
-
-		@Override
-		public int compareTo(Land o) {
-			return o.getVal() - this.val;
-		}
-	}
-
-	int solution040601(int[][] land) {	// 땅따먹기
-		Land [] sorted = new Land[4];
-		int [] sum = new int [4];
-		sum = land[0];
-		
-		for (int i = 0; i < 4; ++i) {
-			sorted[i] = new Land(i, sum[i]);
-		}
-		
-		Arrays.sort(sorted);
-		
-		for(int row = 1; row < land.length; ++row) {
-			for(int col = 0; col < 4; ++col) {
-				if (sorted[0].getCol() != col)
-					sum[col] = land[row][col] + sorted[0].getVal();
-				else
-					sum[col] = land[row][col] + sorted[1].getVal();
-			}
-			
-			for (int i = 0; i < 4; ++i) {
-				sorted[i] = new Land(i, sum[i]);
-			}
-			
-			Arrays.sort(sorted);
-		}
-		
-		Arrays.sort(sum);
-		
-		return sum[3];
-	}
-
-	boolean solution050401(String[] phone_book) {	// 전화번호 목록
-		boolean answer = true;
-        String prefix;
-        
-        Arrays.sort(phone_book);
-        
-        for(int i = 1; i < phone_book.length && answer; ++i) {
-        	prefix = phone_book[i - 1];
-        	
-        	if (prefix.length() < phone_book[i].length()) {
-        		if (phone_book[i].substring(0, prefix.length()).equals(prefix)) {	// String.startWith()
-        			answer = false;
-        		} else {
-        			++i;
-        		}
-        	}
-        }
-        
-        return answer;
-    }
-	
-	int solution050601(String name) {
-		boolean [] done = new boolean[name.length()];
-		int curIdx = 0;
-		int cnt = 0;
-		int answer = 0;
-		
-		for(int i = 0; i < name.length(); ++i) {
-			if (name.charAt(i) == 'A') {
-				done[i] = true;
-				++cnt;
-			}
-		}
-		
-		while (cnt != name.length()) {
-			
-		}
-		
-		return answer;
-	}
-	
+public class Test {
 	int solution050801(String s) {
 		StringBuilder answer = new StringBuilder();
         int index = 0;
@@ -599,4 +322,125 @@ class Solution {
 
         return answer.toString();
     }
+	
+	public int[] solution050901(String code, String day, String[] data) {
+		TreeMap<String, String> prices = new TreeMap<>();
+
+		String[] splits;
+		for (int i = 0; i < data.length; ++i) {
+			splits = data[i].split("=| ");
+
+			if (splits[3].equals(code))
+				if (splits[5].startsWith(day))
+					prices.put(splits[5], splits[1]);
+		}
+
+		Iterator<Map.Entry<String, String>> it = prices.entrySet().iterator();
+
+		int idx = 0;
+		int[] answer = new int[prices.size()];
+
+		while (it.hasNext()) {
+			answer[idx++] = Integer.parseInt(it.next().getValue());
+		}
+
+		return answer;
+	}
+
+	class Person implements Comparable<Person> {
+		private int id;
+		private int time;
+		private int level;
+
+		Person(int id, int time, int level) {
+			this.id = id;
+			this.time = time;
+			this.level = level;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public int getTime() {
+			return time;
+		}
+
+		public void setTime(int time) {
+			this.time = time;
+		}
+
+		public int getLevel() {
+			return level;
+		}
+
+		public void setLevel(int level) {
+			this.level = level;
+		}
+
+		@Override
+		public int compareTo(Person o) {
+			if (this.time == o.getTime())
+				if (this.level == o.getLevel())
+					return this.id - o.getId();
+				else
+					return this.level - o.getLevel();
+			else
+				return this.time - o.getTime();
+		}
+	}
+
+	public int[] solution0509021(int[] t, int[] r) {
+		LinkedList<Person> lList = new LinkedList<>();
+
+		for (int i = 0; i < t.length; ++i) {
+			lList.add(new Person(i, t[i], r[i]));
+		}
+
+		int[] answer = new int[t.length];
+
+		Person temp;
+
+		for (int i = 0; i < t.length; ++i) {
+			Collections.sort(lList);
+
+			temp = lList.poll();
+			answer[i] = temp.getId();
+
+			for (int j = 0; j < lList.size(); ++j) {
+				if (lList.get(j).getTime() == temp.getTime())
+					lList.get(j).setTime(temp.getTime() + 1);
+				else
+					break;
+			}
+		}
+
+		return answer;
+	}
+
+	public int[] solution0509022(int[] t, int[] r) {
+		PriorityQueue<Person> pQue = new PriorityQueue<>();
+
+		for (int i = 0; i < t.length; ++i) {
+			pQue.add(new Person(i, t[i], r[i]));
+		}
+
+		int[] answer = new int[t.length];
+
+		Person temp;
+
+		for (int i = 0; i < t.length; ++i) {
+			temp = pQue.poll();
+			answer[i] = temp.getId();
+
+			if (!pQue.isEmpty() && temp.getTime() == pQue.peek().getTime())
+				pQue.peek().setTime(temp.getTime() + 1);
+		}
+
+		return answer;
+	}
 }
