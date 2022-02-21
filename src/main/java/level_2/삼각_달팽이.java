2aaf4654f169ca3a18d08c2464414fef3d6a9cb6
Kimilm <gnlfla7416@gmail.com>
2022-02-21 16:29:00 +0900

    level_2 / 월간 코드챌린지 시즌1 / 삼각달팽이

---
+++ src/main/java/level_2/삼각_달팽이.java
@@ -0,0 +1,12 @@
+package level_2;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/68645
+ * 월간 코드 챌린지 시즌1 > 삼각 달팽이
+ */
+public class 삼각_달팽이 {
+    public int[] solution(int n) {
+        int[] answer = {};
+        return answer;
+    }
+}
