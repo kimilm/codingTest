39d0dda83ab4a48cb99c885ba9398ad5d1b152a4
kimilm <gnlfla7416@gmail.com>
2022-03-06 16:57:19 +0900

    level_2 / 2017 팁스타운 / 예상 대진표

---
+++ src/main/java/level_2/예상_대진표.java
@@ -0,0 +1,13 @@
+package level_2;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/12985
+ * 2017 팁스타운 > 예상 대진표
+ */
+public class 예상_대진표 {
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+
+        return answer;
+    }
+}
