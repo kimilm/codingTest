6edcf9706ccef4cbc810c99a87e99f4c2468ce4f
kimilm <gnlfla7416@gmail.com>
2022-03-06 17:40:55 +0900

    level_2 / Summer/Winter Coding(2019) / 멀쩡한 사각형

---
+++ src/main/java/level_2/멀쩡한_사각형.java
@@ -0,0 +1,12 @@
+package level_2;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/62048
+ * Summer/Winter Coding(2019) > 멀쩡한 사각형
+ */
+public class 멀쩡한_사각형 {
+    public long solution(int w, int h) {
+        long answer = 1;
+        return answer;
+    }
+}
