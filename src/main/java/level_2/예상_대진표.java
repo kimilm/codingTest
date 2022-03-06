77bb07e2a3539a0b2c1819ead9fb024970c237d1
kimilm <gnlfla7416@gmail.com>
2022-03-06 17:27:26 +0900

    done

--- src/main/java/level_2/예상_대진표.java
+++ src/main/java/level_2/예상_대진표.java
@@ -6,7 +6,13 @@ package level_2;
  */
 public class 예상_대진표 {
     public int solution(int n, int a, int b) {
-        int answer = 0;
+        int answer = 1;
+
+        while ((++a / 2) != (++b / 2)) {
+            ++answer;
+            a /= 2;
+            b /= 2;
+        }
 
         return answer;
     }
