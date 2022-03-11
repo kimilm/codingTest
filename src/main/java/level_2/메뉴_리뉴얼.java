7089c7ac0e031171773f21113b9bd479efdbe984
Kimilm <gnlfla7416@gmail.com>
2022-03-11 17:25:09 +0900

    level_2 / 2021 KAKAO BLIND RECRUITMENT / 메뉴 리뉴얼

---
+++ src/main/java/level_2/메뉴_리뉴얼.java
@@ -0,0 +1,12 @@
+package level_2;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/72411
+ * 2021 KAKAO BLIND RECRUITMENT > 메뉴 리뉴얼
+ */
+public class 메뉴_리뉴얼 {
+    public String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+        return answer;
+    }
+}
