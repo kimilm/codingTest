edd9d0d6318a881c7661f838285c8c67eb20240d
kimilm <gnlfla7416@gmail.com>
2022-03-15 18:59:18 +0900

    상하좌우

--- src/main/java/this_is_coding_test/ch04/Implementation.java
+++ src/main/java/this_is_coding_test/ch04/Implementation.java
@@ -2,5 +2,33 @@ package this_is_coding_test.ch04;
 
 public class Implementation {
 
+    /**
+     * 난이도 하
+     * 시간제한 1초
+     * 메모리제한 128MB
+     * 1 <= N <= 100
+     * 1 <= 이동 횟수 <= 100
+     * x y 좌표를 공백으로 구분하여 출력
+     */
+    public String 상하좌우(int N, String travels) {
+        int x = 1;
+        int y = 1;
+        String[] travelArray = travels.split(" ");
+
+        for (String travel : travelArray) {
+            if (travel.equals("L") && y > 1) {
+                --y;
+            } else if (travel.equals("R") && y < N) {
+                ++y;
+            } else if (travel.equals("U") && x > 1) {
+                --x;
+            } else if (travel.equals("D") && x < N) {
+                ++x;
+            }
+        }
+
+        return x + " " + y;
+    }
+
 
 }
--- src/test/java/this_is_coding_test/ch04/ImplementationTest.java
+++ src/test/java/this_is_coding_test/ch04/ImplementationTest.java
@@ -4,5 +4,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ImplementationTest {
+    private final Implementation imp = new Implementation();
 
+    @Test
+    void 상하좌우() {
+        String result = imp.상하좌우(5, "R R R U D D");
+        String answer = "3 4";
+
+        Assertions.assertThat(result).isEqualTo(answer);
+    }
 }
