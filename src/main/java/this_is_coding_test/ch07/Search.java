6c014948e66549db8012a48cf246e5c425a237d5
kimilm <gnlfla7416@gmail.com>
2022-03-18 21:41:18 +0900

    떡볶이 떡 만들기2

--- src/main/java/this_is_coding_test/ch07/Search.java
+++ src/main/java/this_is_coding_test/ch07/Search.java
@@ -106,7 +106,7 @@ public class Search {
     /**
      * 난이도 중
      * 제한) 시간: 2초, 메모리: 128MB
-     * 1 <= N <= 1_000_000
+     * 2 <= N <= 1_000_000
      * 1 <= M <= 2_000_000_000
      */
     public int 떡볶이_떡_만들기(int n, int m, String tteoks) {
@@ -129,5 +129,50 @@ public class Search {
         }
 
         return height;
+    }
+
+    /**
+     * 이런 유형을 파라메트릭 서치 유형이라 부른다.
+     * 일반적으로 이진탐색을 이용하여 해결한다.
+     * 매우 큰 수를 보면 당연하게 이진탐색을 떠올리도록 하자
+     * 10억 -> 이진탐색 이용 -> 31번 만에 모든 경우의 수 고려 가능
+     * 떡의 개수가 최대 100만 -> 최악의 경우에도 약 3천만 번의 연산 -> 2초 언저리에 연산 완료 가능
+     */
+    public int 떡볶이_떡_만들기_2(int n, int m, String tteoks) {
+        int[] array = parseIntArray(tteoks);
+
+        // 이진 탐색을 위한 시작과 끝 설정
+        int start = 0;
+        int end = Arrays.stream(array).max().orElseThrow();
+
+        int result = 0;
+
+        // 이진 탐색
+        while(start <= end) {
+            int total = 0;
+            int mid = (start + end) / 2;
+
+            // 잘랐을 때 얻는 떡의 양 계산
+            for (int tteok : array) {
+                if (tteok > mid) {
+                    total += tteok - mid;
+                }
+            }
+
+            // 얻은 양이 적다면
+            if (total < m) {
+                // 끝 지점을 당김
+                end = mid - 1;
+            }
+            // 얻은 양이 충분하다면
+            else {
+                // 최대한 덜 잘랐을 때가 정답임, 여기서 저장
+                result = mid;
+                // 시작 지점을 당김
+                start = mid + 1;
+            }
+        }
+
+        return result;
     }
 }
