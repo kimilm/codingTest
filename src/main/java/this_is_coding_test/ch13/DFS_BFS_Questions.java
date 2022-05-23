e404ed3469e0c1432c849660f872b5a7d0a00b6b
kimilm <gnlfla7416@gmail.com>
2022-05-23 20:28:08 +0900

    특정 거리의 도시 찾기 해답지 풀이

--- src/main/java/this_is_coding_test/ch13/DFS_BFS_Questions.java
+++ src/main/java/this_is_coding_test/ch13/DFS_BFS_Questions.java
@@ -54,4 +54,52 @@ public class DFS_BFS_Questions {
         }
         return answer;
     }
+
+    /**
+     * 모든 도로의 길이가 1이라는 조건을 이용해서 너비 우선 탐색으로 빠르게 풀 수 있다.
+     * 노드의 개수 N은 최대 300_000개, 간선의 개수 M은 최대 1_000_000
+     * 시간복잡도 O(N + M) 해결 가능
+     * 특정한 도시 x를 시작점으로 bfs를 수행하여 모든 도시까지의 최단거리 계산, 거리가 K인 도시 출력
+     * 내 풀이는 거리 계산을 비효율적으로 수행했다.
+     */
+
+    public int[] 특정_거리의_도시_찾기_2(int n, int m, int k, int x, String[] cities) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (String city : cities) {
+            int[] node = Arrays.stream(city.split(" "))
+                    .mapToInt(Integer::parseInt).toArray();
+            graph.get(node[0]).add(node[1]);
+        }
+
+        // 모든 도시 최단거리 초기화
+        int[] distance = new int[n + 1];
+        Arrays.fill(distance, -1);
+        distance[x] = 0;
+
+        // bfs 수행
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(x);
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            // 현재 도시에서 이동 가능한 모든 도시
+            for (int city : graph.get(now)) {
+                // 아직 방문하지 않은 도시라면
+                if (distance[city] == -1) {
+                    distance[city] = distance[now] + 1;
+                    queue.add(city);
+                }
+            }
+        }
+
+        // 출력
+        int[] answer = IntStream.range(0, n + 1).filter(i -> distance[i] == k).toArray();
+        if (answer.length == 0) {
+            return new int[]{-1};
+        }
+        return answer;
+    }
 }
--- src/test/java/this_is_coding_test/ch13/DFS_BFS_QuestionsTest.java
+++ src/test/java/this_is_coding_test/ch13/DFS_BFS_QuestionsTest.java
@@ -14,27 +14,27 @@ class DFS_BFS_QuestionsTest {
     Stream<DynamicTest> 특정_거리의_도시_찾기() {
         return Stream.of(
                 DynamicTest.dynamicTest("예제 입력 1", () -> {
-                    int[] result = dbq.특정_거리의_도시_찾기(4, 4, 2, 1, new String[]{"1 2", "1 3", "2 3", "2 4"});
+                    int[] result = dbq.특정_거리의_도시_찾기_2(4, 4, 2, 1, new String[]{"1 2", "1 3", "2 3", "2 4"});
                     int[] answer = new int[] {4};
                     Assertions.assertThat(result).containsExactly(answer);
                 }),
                 DynamicTest.dynamicTest("예제 입력 2", () -> {
-                    int[] result = dbq.특정_거리의_도시_찾기(4, 3, 2, 1, new String[]{"1 2", "1 3", "1 4"});
+                    int[] result = dbq.특정_거리의_도시_찾기_2(4, 3, 2, 1, new String[]{"1 2", "1 3", "1 4"});
                     int[] answer = new int[] {-1};
                     Assertions.assertThat(result).containsExactly(answer);
                 }),
                 DynamicTest.dynamicTest("예제 입력 3", () -> {
-                    int[] result = dbq.특정_거리의_도시_찾기(4, 4, 1, 1, new String[]{"1 2", "1 3", "2 3", "2 4"});
+                    int[] result = dbq.특정_거리의_도시_찾기_2(4, 4, 1, 1, new String[]{"1 2", "1 3", "2 3", "2 4"});
                     int[] answer = new int[] {2, 3};
                     Assertions.assertThat(result).containsExactly(answer);
                 }),
                 DynamicTest.dynamicTest("예제 입력 4", () -> {
-                    int[] result = dbq.특정_거리의_도시_찾기(4, 5, 3, 1, new String[]{"1 2", "1 3", "2 3", "2 4", "4 1"});
+                    int[] result = dbq.특정_거리의_도시_찾기_2(4, 5, 3, 1, new String[]{"1 2", "1 3", "2 3", "2 4", "4 1"});
                     int[] answer = new int[] {-1};
                     Assertions.assertThat(result).containsExactly(answer);
                 }),
                 DynamicTest.dynamicTest("예제 입력 5", () -> {
-                    int[] result = dbq.특정_거리의_도시_찾기(7, 6, 2, 1, new String[]{"1 2", "1 3", "2 4", "2 5", "3 6", "3 7"});
+                    int[] result = dbq.특정_거리의_도시_찾기_2(7, 6, 2, 1, new String[]{"1 2", "1 3", "2 4", "2 5", "3 6", "3 7"});
                     int[] answer = new int[] {4, 5, 6, 7};
                     Assertions.assertThat(result).containsExactly(answer);
                 })
