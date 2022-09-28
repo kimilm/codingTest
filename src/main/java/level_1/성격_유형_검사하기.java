3192be588882603e047510af2a29fa2c7c396f2b
kimilm <gnlfla7416@gmail.com>
2022-09-29 05:03:19 +0900

    refactor: L1_성격_유형_검사하기

--- src/main/java/level_1/성격_유형_검사하기.java
+++ src/main/java/level_1/성격_유형_검사하기.java
@@ -7,44 +7,36 @@ import java.util.Map;
 
 //https://school.programmers.co.kr/learn/courses/30/lessons/118666
 public class 성격_유형_검사하기 {
-    public static List<Map<Integer, String>> TYPE_MAPS;
-    public static Map<String, Integer> COLS;
-    public static Map<String, Integer> ROWS;
+    public static List<Map<Integer, Character>> TYPE_MAPS;
+    public static Map<Character, Integer> COLS;
+    public static Map<Character, Integer> ROWS;
 
     static {
         TYPE_MAPS = new ArrayList<>();
-        TYPE_MAPS.add(Map.of(0, "R", 1, "T"));
-        TYPE_MAPS.add(Map.of(0, "C", 1, "F"));
-        TYPE_MAPS.add(Map.of(0, "J", 1, "M"));
-        TYPE_MAPS.add(Map.of(0, "A", 1, "N"));
+        TYPE_MAPS.add(Map.of(0, 'R', 1, 'T'));
+        TYPE_MAPS.add(Map.of(0, 'C', 1, 'F'));
+        TYPE_MAPS.add(Map.of(0, 'J', 1, 'M'));
+        TYPE_MAPS.add(Map.of(0, 'A', 1, 'N'));
 
         COLS = new HashMap<>();
-        COLS.put("R", 0);
-        COLS.put("T", 1);
-        COLS.put("C", 0);
-        COLS.put("F", 1);
-        COLS.put("J", 0);
-        COLS.put("M", 1);
-        COLS.put("A", 0);
-        COLS.put("N", 1);
+        COLS.put('R', 0);
+        COLS.put('T', 1);
+        COLS.put('C', 0);
+        COLS.put('F', 1);
+        COLS.put('J', 0);
+        COLS.put('M', 1);
+        COLS.put('A', 0);
+        COLS.put('N', 1);
 
         ROWS = new HashMap<>();
-        ROWS.put("R", 0);
-        ROWS.put("T", 0);
-        ROWS.put("RT", 0);
-        ROWS.put("TR", 0);
-        ROWS.put("C", 1);
-        ROWS.put("F", 1);
-        ROWS.put("CF", 1);
-        ROWS.put("FC", 1);
-        ROWS.put("J", 2);
-        ROWS.put("M", 2);
-        ROWS.put("JM", 2);
-        ROWS.put("MJ", 2);
-        ROWS.put("A", 3);
-        ROWS.put("N", 3);
-        ROWS.put("AN", 3);
-        ROWS.put("NA", 3);
+        ROWS.put('R', 0);
+        ROWS.put('T', 0);
+        ROWS.put('C', 1);
+        ROWS.put('F', 1);
+        ROWS.put('J', 2);
+        ROWS.put('M', 2);
+        ROWS.put('A', 3);
+        ROWS.put('N', 3);
     }
 
     public String solution(String[] survey, int[] choices) {
@@ -55,15 +47,11 @@ public class 성격_유형_검사하기 {
             int choice = choices[i] - 4;
 
             if (choice != 0) {
-                String[] personal = survey[i].split("");
+                char type = choice < 0
+                        ? survey[i].charAt(0)
+                        : survey[i].charAt(1);
 
-                String type = choice < 0
-                        ? personal[0]
-                        : personal[1];
-
-                Integer integer = ROWS.get(survey[i]);
-                Integer integer1 = COLS.get(type);
-                types[integer][integer1] += Math.abs(choice);
+                types[ROWS.get(type)][COLS.get(type)] += Math.abs(choice);
             }
         }
 
@@ -80,3 +68,29 @@ public class 성격_유형_검사하기 {
         return sb.toString();
     }
 }
+
+/**
+ * 전                                        후
+ * 테스트 1 〉	통과 (0.21ms, 75.5MB)        테스트 1 〉	통과 (0.06ms, 78.7MB)
+ * 테스트 2 〉	통과 (0.16ms, 78MB)          테스트 2 〉	통과 (0.07ms, 73.6MB)
+ * 테스트 3 〉	통과 (0.25ms, 77.1MB)        테스트 3 〉	통과 (0.06ms, 74.6MB)
+ * 테스트 4 〉	통과 (0.29ms, 88.7MB)        테스트 4 〉	통과 (0.08ms, 77.1MB)
+ * 테스트 5 〉	통과 (0.23ms, 77.3MB)        테스트 5 〉	통과 (0.07ms, 76.4MB)
+ * 테스트 6 〉	통과 (0.23ms, 73.1MB)        테스트 6 〉	통과 (0.10ms, 75.1MB)
+ * 테스트 7 〉	통과 (0.59ms, 73.1MB)        테스트 7 〉	통과 (0.08ms, 75.1MB)
+ * 테스트 8 〉	통과 (0.76ms, 77.5MB)        테스트 8 〉	통과 (0.10ms, 72.9MB)
+ * 테스트 9 〉	통과 (1.16ms, 77.6MB)        테스트 9 〉	통과 (0.10ms, 84.6MB)
+ * 테스트 10 〉	통과 (1.36ms, 70.9MB)        테스트 10 〉	통과 (0.11ms, 78MB)
+ * 테스트 11 〉	통과 (1.43ms, 72.8MB)        테스트 11 〉	통과 (0.12ms, 88.2MB)
+ * 테스트 12 〉	통과 (3.55ms, 70.2MB)        테스트 12 〉	통과 (0.28ms, 73.4MB)
+ * 테스트 13 〉	통과 (5.61ms, 75.4MB)        테스트 13 〉	통과 (0.34ms, 75.3MB)
+ * 테스트 14 〉	통과 (5.84ms, 77.2MB)        테스트 14 〉	통과 (0.41ms, 77.7MB)
+ * 테스트 15 〉	통과 (6.07ms, 81.9MB)        테스트 15 〉	통과 (0.50ms, 78.8MB)
+ * 테스트 16 〉	통과 (6.40ms, 75.1MB)        테스트 16 〉	통과 (0.55ms, 78.8MB)
+ * 테스트 17 〉	통과 (6.21ms, 81.4MB)        테스트 17 〉	통과 (0.48ms, 77.4MB)
+ * 테스트 18 〉	통과 (6.62ms, 81.9MB)        테스트 18 〉	통과 (0.46ms, 76.4MB)
+ * 테스트 19 〉	통과 (7.27ms, 85.6MB)        테스트 19 〉	통과 (0.52ms, 74MB)
+ * 테스트 20 〉	통과 (5.90ms, 80.8MB)        테스트 20 〉	통과 (0.42ms, 75.1MB)
+ *
+ * split -> charAt 으로 변경, 가장 느린 테스트 케이스에서 10배의 성능 차이가 있었음
+ */
