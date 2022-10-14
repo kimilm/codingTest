aff1ed543e761f9d907bbd17504ac993dda63a3c
kimilm <gnlfla7416@gmail.com>
2022-10-14 15:19:18 +0900

    refactor: L1_숫자_짝꿍

--- src/main/java/level_1/숫자_짝꿍.java
+++ src/main/java/level_1/숫자_짝꿍.java
@@ -1,35 +1,29 @@
 package level_1;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 // https://school.programmers.co.kr/learn/courses/30/lessons/131128?language=java
 public class 숫자_짝꿍 {
     public String solution(String X, String Y) {
         int[] array = new int[10];
-        List<Integer> numbers = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         X.codePoints().forEach(cp -> ++array[cp - 48]);
-        Y.codePoints().forEach(cp -> {
+        Y.codePoints().sorted().forEach(cp -> {
             int idx = cp - 48;
             if (array[idx] > 0) {
                 --array[idx];
-                numbers.add(idx);
+                sb.append(idx);
             }
         });
 
-        if (numbers.isEmpty()) {
+        sb.reverse();
+
+        if (sb.length() == 0) {
             return "-1";
         }
 
-        if (numbers.get(0) == 0) {
+        if (sb.charAt(0) == '0') {
             return "0";
         }
 
-        return numbers.stream()
-                .sorted(Collections.reverseOrder())
-                .map(String::valueOf)
-                .collect(Collectors.joining());
+        return sb.toString();
     }
 }
