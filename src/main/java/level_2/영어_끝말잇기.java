1814e2c84b3976a95ec8468ee6d5504f4756f7aa
kimilm <gnlfla7416@gmail.com>
2022-10-19 18:24:14 +0900

    refactor: L2_영어_끝말잇기

--- src/main/java/level_2/영어_끝말잇기.java
+++ src/main/java/level_2/영어_끝말잇기.java
@@ -7,22 +7,17 @@ import java.util.Set;
 public class 영어_끝말잇기 {
     public int[] solution(int n, String[] words) {
         Set<String> wordSet = new HashSet<>();
-        int person = 1;
-        int round = 1;
         char prevLastChar = words[0].charAt(0);
 
+        int count = 0;
         for (String word : words) {
             if (wordSet.contains(word) || word.charAt(0) != prevLastChar) {
-                return new int[]{person, round};
+                return new int[]{count % n + 1, count / n + 1};
             }
 
+            ++count;
             wordSet.add(word);
             prevLastChar = word.charAt(word.length() - 1);
-
-            if (++person > n) {
-                person = 1;
-                ++round;
-            }
         }
 
         return new int[]{0, 0};
