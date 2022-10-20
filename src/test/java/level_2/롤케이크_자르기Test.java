b4331dc47a927a3b9bec670a94bcca55690769cd
kimilm <gnlfla7416@gmail.com>
2022-10-20 19:45:52 +0900

    test: add test case

--- src/test/java/level_2/롤케이크_자르기Test.java
+++ src/test/java/level_2/롤케이크_자르기Test.java
@@ -11,7 +11,31 @@ class 롤케이크_자르기Test {
     void solution() {
         int result = r.solution(new int[]{1, 2, 1, 3, 1, 4, 1, 2});
         int answer = 2;
-        Assertions.assertThat(result).isEqualTo(2);
+        Assertions.assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    void solution_test2() {
+        int result = r.solution(new int[]{1, 1, 1, 1, 1, 1, 1});
+        int answer = 6;
+
+        Assertions.assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    void solution_test3() {
+        int result = r.solution(new int[]{1, 2, 3, 1, 4});
+        int answer = 0;
+
+        Assertions.assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    void solution_test4() {
+        int result = r.solution(new int[]{1, 1, 1, 1, 2, 2});
+        int answer = 1;
+
+        Assertions.assertThat(result).isEqualTo(answer);
     }
 
     @Test
