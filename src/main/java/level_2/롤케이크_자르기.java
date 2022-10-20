6c62ec92d740c238f0931978bbbec58a2a0a9148
kimilm <gnlfla7416@gmail.com>
2022-10-20 19:34:11 +0900

    refactor: solution 01) 9 fail, 95/100

--- src/main/java/level_2/롤케이크_자르기.java
+++ src/main/java/level_2/롤케이크_자르기.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/132265
 public class 롤케이크_자르기 {
-    // 1, 9, 19 fail, 85/100
+    // 9 fail, 95/100
     public int solution(int[] topping) {
         Map<Integer, Integer> frontTopping = new LinkedHashMap<>();
         Map<Integer, Integer> backTopping = new LinkedHashMap<>();
@@ -23,14 +23,10 @@ public class 롤케이크_자르기 {
 
         List<Integer> frontValues = new ArrayList<>(frontTopping.values());
         List<Integer> backValues = new ArrayList<>(backTopping.values());
-        int answer = 0;
+        int answer = (int)1e9;
 
-        for (int i = frontValues.size() - 1; i > -1; --i) {
-            answer = backValues.get(i) - frontValues.get(i);
-
-            if (answer > -1) {
-                break;
-            }
+        for (int i = 0; i < frontValues.size(); i++) {
+            answer = Integer.min(answer, Math.abs(frontValues.get(i) - backValues.get(i)));
         }
 
         return answer;
