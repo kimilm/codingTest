f6c0ab54cf43d0bc85936d1b9de258d749fe7b7b
kimilm <gnlfla7416@gmail.com>
2022-10-23 19:58:06 +0900

    refactor: 보트에 태우는 조건 변경

--- src/main/java/level_2/구명보트.java
+++ src/main/java/level_2/구명보트.java
@@ -4,33 +4,20 @@ import java.util.Arrays;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/42885
 public class 구명보트 {
+    // 보트에 탈 수 있는 사람은 최대 두명
     public int solution(int[] people, int limit) {
         Arrays.sort(people);
 
-        int sum = Arrays.stream(people).sum();
         int answer = 0;
-        int left = limit;
         int head = 0;
         int tail = people.length - 1;
 
-        while(true) {
-            if (sum <= limit) {
-                ++answer;
-                break;
+        while(head <= tail) {
+            if (people[head] + people[tail] <= limit) {
+                ++head;
             }
-
-            int last = people[tail--];
-            left -= last;
-            sum -= last;
-
-            while(people[head] <= left) {
-                int first = people[head++];
-                left -= first;
-                sum -= first;
-            }
-
+            --tail;
             ++answer;
-            left = limit;
         }
 
         return answer;
